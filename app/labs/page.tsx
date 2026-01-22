2be7a80d6487cc8cfbdc9e5810a047a6220d576e
kebrcic <brcicsutton.kevin@gmail.com>
2026-01-21 20:19:20 -0500

    Added Name to labs home page

--- app/labs/page.tsx
+++ app/labs/page.tsx
@@ -4,6 +4,7 @@ export default function labs() {
     <div id="wd-labs">
       <h1>Labs</h1>
       <ul>
+        <li>Author: Kevin Brcic Sutton</li>
         <li>
           <Link href="/labs/lab1" id="wd-lab1-link">
             Lab 1: HTML Examples{" "}
@@ -17,6 +18,11 @@ export default function labs() {
         <li>
           <Link href="/labs/lab3" id="wd-lab3-link">
             Lab 3: JavaScript Fundamentals{" "}
+          </Link>
+        </li>
+        <li>
+          <Link href="/" id="wd-kambaz-link">
+            Kambaz
           </Link>
         </li>
       </ul>
