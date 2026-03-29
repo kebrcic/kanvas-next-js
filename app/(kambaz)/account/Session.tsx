347a09c998aa8d98f93657b7e422c3bb75cf7d63
kebrcic <brcicsutton.kevin@gmail.com>
2026-03-29 17:18:51 -0400

    finished frontend

--- app/(kambaz)/account/Session.tsx
+++ app/(kambaz)/account/Session.tsx
@@ -1,8 +1,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable react-hooks/set-state-in-effect */
 import * as client from "./client";
+import * as courseClient from "../courses/client";
 import { useEffect, useState } from "react";
 import { setCurrentUser } from "./reducer";
+import { setEnrollments } from "../enrollments/reducer";
 import { useDispatch } from "react-redux";
 export default function Session({ children }: { children: any }) {
   const [pending, setPending] = useState(true);
@@ -11,6 +13,8 @@ export default function Session({ children }: { children: any }) {
     try {
       const currentUser = await client.profile();
       dispatch(setCurrentUser(currentUser));
+      const enrollments = await courseClient.findEnrollmentsForUser();
+      dispatch(setEnrollments(enrollments));
     } catch (err: any) {
       console.error(err);
     }
